257cabb2f3da1320bf4033858825af29e4a3ed08
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-20 06:05:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  kittens.slice(0, kittens.length - 1)
+  var kittens = kittens.slice(0, kittens.length - 1)
   return kittens
 }
 
