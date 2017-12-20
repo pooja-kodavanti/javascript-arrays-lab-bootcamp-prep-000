aefe7ee26b07fde07e632a96250bd0216380c6ca
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-20 06:05:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,52 +25,23 @@ function destructivelyRemoveLastKitten() {
 function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
-function Arrays() {
-  window.kittens = ['Milo', 'Otis', 'Garfield']
-}
-
-function kittens() {
-  var kittens = ["Milo", "Otis", "Garfield"]
-}
 
 function appendKitten(name) {
-  var kittens = ["Milo", "Otis", "Garfield"]
   var name = 'Broom'
   return [...kittens, name]
 }
-function Arrays() {
-  window.kittens = ['Milo', 'Otis', 'Garfield']
-}
 
-function kittens() {
-  var kittens = ["Milo", "Otis", "Garfield"]
-}
 function prependKitten(name) {
-  var kittens = ["Milo", "Otis", "Garfield"]
   var name = 'Arnold'
   return [name, ...kittens]
 }
-function Arrays() {
-  window.kittens = ['Milo', 'Otis', 'Garfield']
-}
 
-function kittens() {
-  var kittens = ["Milo", "Otis", "Garfield"]
-}
 function removeLastKitten() {
-//  var kittens = ["Milo", "Otis", "Garfield"]
   kittens.slice(0, kittens.length - 1)
   return kittens
 }
-function Arrays() {
-  window.kittens = ['Milo', 'Otis', 'Garfield']
-}
 
-function kittens() {
-  var kittens = ["Milo", "Otis", "Garfield"]
-}
 function removeFirstKitten() {
-  var kittens = ["Milo", "Otis", "Garfield"]
   kittens.slice(1)
   return kittens
 }
