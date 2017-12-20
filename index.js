f28bdbc6d636aaaa664a520e5554de3b937d81a2
Pooja Kodavanti <pkodavan@gmail.com>
2017-12-20 06:00:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 const app = "I don't do much."
+
+function Arrays() {
+  window.kittens = ['Milo', 'Otis', 'Garfield']
+}
+
+function kittens() {
+  var kittens = ["Milo", "Otis", "Garfield"]
+}
+
+function destructivelyAppendKitten(name) {
+  var name = 'Ralph'
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  var name = 'Bob'
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var name = 'Broom'
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  var name = 'Arnold'
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  kittens.slice(0, kittens.length - 1)
+  return kittens
+}
+
+function removeFirstKitten() {
+  kittens.slice(1)
+  return kittens
+}
